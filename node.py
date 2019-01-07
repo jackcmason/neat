087cdedf1e1f2f972c7f60ba5d8624b4a89f7892
Jack Mason <jack.mason@posteo.net>
2019-01-07 12:46:03 +1000

    implemented string representation of Node

--- node.py
+++ node.py
@@ -1,4 +1,8 @@
 class Node:
-    def __init__(self, number, layer):
+    def __init__(self, number, layer, nodeType):
         self.number = number
         self.layer = layer
+        self.nodeType = nodeType
+
+    def __str__(self):
+        return "Node " + str(self.number) + " is " + self.nodeType + " on layer " + str(self.layer)
