2c45650062cba63d27ab2cf47a281bc44762de56
Jack Mason <jack.mason@posteo.net>
2019-01-07 13:02:08 +1000

    implemented string representation of Connection

--- connect.py
+++ connect.py
@@ -6,3 +6,7 @@ class Connection:
         self.enabled = enabled
         #innovation number
         self.innov = innov
+
+    def __str__(self):
+        status = 'ENABLED' if self.enabled else 'DISABLED'
+        return "Connection " + str(self.innov) + " with weight " + str(self.weight) + " is " + status + "\nconnects Node " + str(self.inNode.number) + " to Node " + str(self.outNode.number)
